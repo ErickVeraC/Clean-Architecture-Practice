193b6cf1b711f1a1e227b02f397590b7614e9b0d
Erick Vera <erickverac90@gmail.com>
2024-08-05 19:39:54 -0600

    Agregando revisor de correo electronico

--- src/models/mentors.model.js
+++ src/models/mentors.model.js
@@ -6,7 +6,14 @@ function Mentor(data) {
     firstName: yup.string().required(),
     lastName: yup.string().required(),
     age: yup.number().min(18).required(),
-    email: yup.string().email().required(),
+    email: yup
+      .string()
+      .email()
+      .matches(
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "This is not a valid email"
+      )
+      .required(),
     phone: yup.string().min(10).max(10).required(),
   });
 
